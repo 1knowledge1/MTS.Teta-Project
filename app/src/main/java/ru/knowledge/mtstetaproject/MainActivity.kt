01d0757a7bef2f6e0fc2bf473f78619fbfcc69ff
Yaroslav <iaroslaw.bondarenko@yandex.ru>
2021-07-09 16:04:56 +0300

    Initial commit.

---
+++ app/src/main/java/ru/knowledge/mtstetaproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.knowledge.mtstetaproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
